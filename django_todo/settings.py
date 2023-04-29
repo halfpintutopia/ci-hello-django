de74c6f706ed5be25452bf929fcab1e6682e7dfd
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-29 09:25:32 +0200

    fix: add host to allowed hosts in settings file

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['q147852-ci-todo-app.herokuapp.com']
 
 # Application definition
 
